adced37c29a3c398d9dbc607804a2764e696b952
khalil <kalilmteixeira@gmail.com>
2023-07-19 14:04:32 -0300

    Atualização do Sistema de conversão Real-Dollar-Real

---
+++ src/com/br/moedas/app.java
@@ -0,0 +1,38 @@
+package com.br.moedas;
+
+
+import java.util.Scanner;
+
+public class app {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int opcao;
+
+        System.out.print("Informe a cotação do dolar: ");
+        String valorCotacao = scn.nextLine();
+        moedas cotacao = new moedas(valorCotacao);
+
+
+        System.out.print("Informe o valor a ser convertido: ");
+        String valorConverter = scn.nextLine();
+        moedas valor = new moedas(valorConverter);
+
+        System.out.println("\n1. Converter valor de Real para Dolar.");
+        System.out.println("2. Converter valor de Dolar para Real.");
+        System.out.print("Informe uma das opções: ");
+        opcao = Integer.parseInt(scn.nextLine());
+
+        String resultado;
+        switch (opcao) {
+            case 1 -> resultado = valor.converterParaDolar(cotacao.getValor());
+            case 2 -> resultado = valor.converterParaReal(cotacao.getValor());
+            default -> {
+                System.out.println("Opção Invalida!!");
+                return;
+            }
+        }
+        System.out.println("O valor é: " + resultado);
+    }
+
+}
---
+++ src/com/br/moedas/moedas.java
@@ -0,0 +1,41 @@
+package com.br.moedas;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+public class moedas {
+
+    private BigDecimal valor;
+    private static final String UNIDADE_REAL = "R$";
+    private static final String UNIDADE_DOLLAR = "U$";
+    private static final DecimalFormat REAL = new DecimalFormat(UNIDADE_REAL + " #,###,##0.00");
+    private static final DecimalFormat DOLLAR = new DecimalFormat(UNIDADE_DOLLAR + " #,###,##0.00");
+
+    public moedas(String valor) {
+        this.valor = new BigDecimal(valor);
+    }
+
+    public BigDecimal getValor(){
+        return valor;
+    }
+
+    public String getUnidadeReal() {
+        return REAL.format(valor);
+    }
+
+    public String getUnidadeDollar() {
+        return DOLLAR.format(valor);
+    }
+    public String converterParaDolar(BigDecimal cotacao) {
+        BigDecimal valorEmDolares = valor.divide(cotacao, 2, BigDecimal.ROUND_HALF_UP);
+        return DOLLAR.format(valorEmDolares);
+    }
+
+    public String converterParaReal(BigDecimal cotacao){
+        BigDecimal valorEmReais = valor.multiply(cotacao).setScale(2,BigDecimal.ROUND_HALF_EVEN);
+        return REAL.format(valorEmReais);
+    }
+
+}
+
+
